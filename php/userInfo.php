349b8befac98e2242489adb9708ffcf3e56f675e
eimingming <rapeepan.jk@mail.kmutt.ac.th>
2021-05-11 01:36:15 +0700

    add file and edit

---
+++ php/userInfo.php
@@ -0,0 +1,8 @@
+<?php
+    $name = $_POST['name'];
+    $lastName = $_POST['lastName'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+?>
